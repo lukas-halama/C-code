86064441a3039d134a0388fc14d18afe3482217f
lukas <lukas>
2025-10-23 11:09:23 +0200

    c code commit

---
+++ area_of_rectangle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int side_a = 50; 
+int side_b = 40;
+
+int main () {
+
+	int area_a+b = a * b;
+
+	printf("area_a+b\n%d" area_a+b)
+
+	return 0;
+
+
+
+
+
+
+
+}
---
+++ simple_sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int a = 5;
+int b = 10;
+
+int main() {
+	int sum = a + b;	
+	printf("soucet cisel a & b je roven %d\n", sum);
+	return 0;
+}
