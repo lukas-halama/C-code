7c299d1b8f21f943382e9c49cb6e04a19968c41e
lukas <lukas>
2025-10-24 08:35:14 +0200

    commit noveho c

---
+++ input.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int input;
+
+int main (){
+
+
+	scanf("%d", &input);
+	printf("tve cislo je: ---->>>> %d\n ", input);
+
+
+
+
+
+}
---
+++ sum_of_3_nums.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int num1;
+int num2;
+int num3;
+
+
+int main (){
+
+	
+
+	scanf("%d", &num1);
+	scanf("%d", &num2);
+	scanf("%d", &num3);
+	
+	int sum = num1+num2+num3;		
+	
+	printf("soucet cisel num1-num3 = %d\n", sum);
+
+
+		
+}
