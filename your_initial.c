f9ad203b3c47337b83e5e355ef4361bf0323752d
lukas <lukas>
2025-10-25 14:31:01 +0200

    your_initial

---
+++ your_initial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+char character;
+
+int main(){
+
+	
+
+	printf("enter one character \n ");
+	
+
+	scanf("%c", &character);
+
+
+	printf("your character is  '%c'\n", character);
+
+	return 0;
+}
