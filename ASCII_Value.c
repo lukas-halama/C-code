c542292ccbd97fddb9977084c4a77e8c7d57c774
lukas <lukas>
2025-10-25 15:04:01 +0200

    novy commit

---
+++ ASCII_Value.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+char character;
+
+int main(){
+
+	
+
+	printf("enter one character \n ");
+	
+
+	scanf("%c", &character);
+
+
+	printf("your character is  '%d'\n", character);
+
+	return 0;
+}
+
