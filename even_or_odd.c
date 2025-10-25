53556b3a01af5c82ab7a6191e43532a5da7355b9
lukas <lukas>
2025-10-25 23:52:36 +0200

    commited

---
+++ even_or_odd.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+
+int num;
+
+int main(){
+
+	printf("enter a number to check if its odd or even\n");
+
+	scanf("%d", &num);
+
+	if (num % 2 == 0) {
+
+		printf("your number is even\n");
+
+
+	}else {
+
+		printf("your number is odd\n");
+
+
+
+	}
+
+
+
+
+
+
+
+
+}
