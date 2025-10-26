d36b3e1e16b559db68f44a2945c19fcad2fe82e4
lukas <lukas>
2025-10-26 17:57:39 +0100

    commit

---
+++ for_loop_countdown.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(){
+
+	for(int i = 10; i >= 1; i--){
+
+		printf("prints out the countdown from 10  %d\n", i);	
+	}
+
+
+
+	return 0;
+}
---
+++ simple_for_loop_counter.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+
+int main(){
+
+
+	for(int i = 1; i <= 10; i++) {
+		printf("prints out the line of numbers	%d\n", i);
+
+
+
+	}
+
+	
+
+
+
+	return 0;
+}
